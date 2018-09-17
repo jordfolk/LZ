2bfc2ca8ced1004f5fd76e0b4c3aa22dba1abec1
Jacob Hytting <jpcxd1@hotmail.com>
2018-09-17 12:53:05 +0200

    Added Login, link to discord, Data access layer

---
+++ LoveZone/LoveZone/DAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveZone
+{
+    class DAL
+    {
+        public static DataTable ExecSP(string spName, List<SqlParameter> sqlParams = null)
+        {
+            string strConnect = "Server=tcp:lovezone.database.windows.net,1433;Initial Catalog=LoveZone;Persist Security Info=False;User ID=LoveZoneAdmin;Password=Passw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlConnection conn = new SqlConnection();
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                conn = new SqlConnection(strConnect);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(spName, conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.AddRange(sqlParams.ToArray());
+
+                SqlCommand command = conn.CreateCommand();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                dt.Load(dr);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+    }
+}
---
+++ LoveZone/LoveZone/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoveZone
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Password = Password_PasswordBox.Password;
+
+            List<SqlParameter> sqlParams = new List<SqlParameter>
+            {
+                new SqlParameter("Brugernavn", Brugernavn_Textbox.Text),
+                new SqlParameter("Password", Password)
+            };
+
+            DataTable dtLoginResultsAdmin = DAL.ExecSP("ValidateLogin", sqlParams);
+
+            if (dtLoginResultsAdmin.Rows.Count == 1)
+            {
+                LoveZone.MainWindow win2 = new LoveZone.MainWindow();
+                win2.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Dit brugernavn eller password er forkert");
+            }
+        }
+    }
+}
