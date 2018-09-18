8f57fd55effe6e66f37599b8ac9ad3d98c4fc790
oliverornkow <34339613+oliverornkow@users.noreply.github.com>
2018-09-18 11:16:42 +0200

    Datagrid !!

--- LoveZone/LoveZone/Find_match.xaml.cs
+++ LoveZone/LoveZone/Find_match.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace LoveZone
 {
@@ -20,9 +22,30 @@ namespace LoveZone
     /// </summary>
     public partial class Find_match : Page
     {
+        
         public Find_match()
         {
             InitializeComponent();
+            BrugerOversigt();
+        }
+        private void BrugerOversigt()
+        {
+            string connectionString = @"Data Source=LoveZone.Database.Windows.Net;Initial Catalog=LoveZone;Persist Security Info=True;User ID=LoveZoneAdmin;Password=Passw0rd";
+            //@"Data Source=DESKTOP-CM5VBFO\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=BGBank";
+            //String connectionString = @"Data source=(LocalDB)\LocalDB;Initial Catalog=BGBank;Persist Security Info=True;User ID=SQLAdmin;Password=Passw0rd";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("select * from Bruger_oplysninger", con);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            Oversigt.IsReadOnly = true;
+            Oversigt.ItemsSource = dt.DefaultView;
+            //DV = dt.DefaultView;
+            //DataContext = this;
+
+            cmd.Dispose();
+            con.Close();
         }
     }
 }
